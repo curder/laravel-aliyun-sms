08110000939626451947d234776297fd1f6f5225
curd <curder@foxmail.com>
2017-07-03 16:43:53 +0800

    typo fix

--- src/AliyunSms.php
+++ src/AliyunSms.php
@@ -11,22 +11,33 @@ use Curder\AliyunCore\Exception\ServerException;
 class AliyunSms {
     public function send($mobile, $tplId, $params)
     {
-        define('ENABLE_HTTP_PROXY', env('ALIYUN_SMS_ENABLE_HTTP_PROXY', false));
-        define('HTTP_PROXY_IP',     env('ALIYUN_SMS_HTTP_PROXY_IP', '127.0.0.1'));
-        define('HTTP_PROXY_PORT',   env('ALIYUN_SMS_HTTP_PROXY_PORT', '8888'));
-        $endpoint = new Endpoint(config('aliyunsms.region_id'), EndpointConfig::getregionIds(), EndpointConfig::getProducDomains());
-        $endpoints = array($endpoint);
-        EndpointProvider::setEndpoints($endpoints);
-        $iClientProfile = DefaultProfile::getProfile(config('aliyunsms.region_id'), config('aliyunsms.access_key'), config('aliyunsms.access_secret'));
-        $client = new DefaultAcsClient($iClientProfile);
-        $request = new SendSmsRequest();
-        $request->setSignName(config('aliyunsms.sign_name')); /*签名名称*/
-        $request->setTemplateCode($tplId);                /*模板code*/
-        $request->setRecNum($mobile);                     /*目标手机号*/
-        $request->setParamString(json_encode($params));/*模板变量，数字一定要转换为字符串*/
+        //此处需要替换成自己的AK信息
+        $accessKeyId = config('aliyunsms.access_key', "LTAIBF46CdQmEnsE");//参考本文档步骤2
+        $accessKeySecret = config('aliyunsms.access_secret',"GC3uDFXWKO9GgZJxZ8Lp5Ym0aPEfmc");//参考本文档步骤2
+        //暂时不支持多Region
+        $region = config('aliyunsms.region_id',"cn-hangzhou");
+         //短信API产品名
+        $product = "Dysmsapi";
+        //短信API产品域名
+        $domain = "dysmsapi.aliyuncs.com";
+        //初始化访问的acsCleint
+        $profile = DefaultProfile::getProfile($region, $accessKeyId, $accessKeySecret);
+        DefaultProfile::addEndpoint("cn-hangzhou", "cn-hangzhou", $product, $domain);
+        $acsClient= new DefaultAcsClient($profile);
+        $request = new SendSmsRequest;
+        //必填-短信接收号码。支持以逗号分隔的形式进行批量调用，批量上限为20个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
+        $request->setPhoneNumbers("18801212173");
+        //必填-短信签名
+        $request->setSignName(config('aliyunsms.sign_name','北京和中')); /*签名名称*/
+        //必填-短信模板Code
+        $request->setTemplateCode($tplId);/*模板code*/
+        //选填-假如模板中存在变量需要替换则为必填(JSON格式)
+        $request->setTemplateParam(json_encode($params)); // "{\"code\":\"12345\",\"minutes\":\"5\"}"
+        //选填-发送短信流水号
+        $request->setOutId("1234");
         try {
-            $response = $client->getAcsResponse($request);
-            return $response;
+            $acsResponse = $acsClient->getAcsResponse($request);
+            return $acsResponse;
         } catch (ClientException  $e) {
             logger()->error('客户端错误');
             logger()->error($e->getErrorCode());
