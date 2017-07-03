e3897ff326daba282fdb25e7ccd2610431728dc1
curd <curder@foxmail.com>
2017-07-03 18:21:37 +0800

    typo fix

--- src/AliyunSms.php
+++ src/AliyunSms.php
@@ -9,7 +9,14 @@ use Curder\AliyunSms\Request\V20170525\SendSmsRequest;
 use Curder\AliyunCore\Exception\ClientException;
 use Curder\AliyunCore\Exception\ServerException;
 class AliyunSms {
-    public function send($mobile, $tplId, $params)
+    /**
+     * 发送短信
+     * @param  string $mobile 接受短信的号码
+     * @param  string $tplId  签名id，请在阿里云后台获取
+     * @param  array $params 发送的参数
+     * @return mixed         返回错误或者数组信息
+     */
+    public function send($mobile, $tplId, $params = array())
     {
         // 引用配置文件
         include_once dirname(dirname(dirname(__FILE__))) . '/aliyun-core/src/Config.php';
@@ -28,9 +35,9 @@ class AliyunSms {
         $acsClient= new DefaultAcsClient($profile);
         $request = new SendSmsRequest;
         //必填-短信接收号码。支持以逗号分隔的形式进行批量调用，批量上限为20个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
-        $request->setPhoneNumbers("18801212173");
+        $request->setPhoneNumbers($mobile);
         //必填-短信签名
-        $request->setSignName(config('aliyunsms.sign_name','北京和中')); /*签名名称*/
+        $request->setSignName(config('aliyunsms.sign_name')); /*签名名称*/
         //必填-短信模板Code
         $request->setTemplateCode($tplId);/*模板code*/
         //选填-假如模板中存在变量需要替换则为必填(JSON格式)
